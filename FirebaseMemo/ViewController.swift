9c93cfeff85cbf485777b1b63e370799566e0be2
leo-30 <mio.rabbit971.snow@gmail.com>
2019-12-05 17:40:56 +0900

    Initial Commit

---
+++ FirebaseMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseMemo
+//
+//  Created by 原田澪 on 2019/12/05.
+//  Copyright © 2019 MioHarada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
